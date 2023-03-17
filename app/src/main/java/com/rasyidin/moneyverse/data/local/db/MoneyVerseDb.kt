739f1fa9a9b3f52ff229720d45dd6e1a2bc427e0
RASYIDIN\rafli <raflirasyidin20@gmail.com>
2023-03-18 01:26:35 +0700

    #865by0brp fix populate data

--- app/src/main/java/com/rasyidin/moneyverse/data/local/db/MoneyVerseDb.kt
+++ app/src/main/java/com/rasyidin/moneyverse/data/local/db/MoneyVerseDb.kt
@@ -1,6 +1,7 @@
 package com.rasyidin.moneyverse.data.local.db
 
 import android.content.Context
+import androidx.compose.ui.graphics.toArgb
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -15,6 +16,7 @@ import com.rasyidin.moneyverse.data.local.entities.transaction.TransactionDao
 import com.rasyidin.moneyverse.data.local.entities.transaction.TransactionEntity
 import com.rasyidin.moneyverse.domain.model.category.CategoryKey
 import com.rasyidin.moneyverse.domain.model.category.CategoryType
+import com.rasyidin.moneyverse.ui.theme.*
 import com.rasyidin.moneyverse.utils.DateUtils
 
 @Database(
@@ -46,7 +48,7 @@ abstract class MoneyVerseDb : RoomDatabase() {
                     nominal = 0,
                     updatedAt = DateUtils.getCurrentDate(),
                     iconPath = R.drawable.ic_cash,
-                    bgColor = R.color.color_bg_green,
+                    bgColor = ColorBgGreen.toArgb(),
                     desc = ""
                 )
             )
@@ -55,37 +57,37 @@ abstract class MoneyVerseDb : RoomDatabase() {
         fun initCategories(context: Context): List<CategoryEntity> {
             val categories = mutableListOf<CategoryEntity>()
             categories.apply {
-                add(CategoryEntity(CategoryKey.FOOD, R.color.color_bg_red, R.drawable.ic_food_n_drink, context.getString(R.string.makan_dan_minum), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.SHOP, R.color.color_bg_light_blue, R.drawable.ic_belanja, context.getString(R.string.belanja), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.TRANSPORT, R.color.color_bg_blue, R.drawable.ic_kendaraan, context.getString(R.string.kendaraan), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.BILL, R.color.color_bg_purple, R.drawable.ic_tagihan, context.getString(R.string.tagihan), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.HEALTH, R.color.color_bg_gray, R.drawable.ic_kesehatan, context.getString(R.string.kesehatan), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.HOLIDAY, R.color.color_bg_green, R.drawable.ic_liburan, context.getString(R.string.liburan), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.PET, R.color.color_bg_pink, R.drawable.ic_peliharaan, context.getString(R.string.peliharaan), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.INSURANCE, R.color.color_bg_purple, R.drawable.ic_asuransi, context.getString(R.string.asuransi), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.ALMS, R.color.color_bg_red, R.drawable.ic_sedekah, context.getString(R.string.sedekah), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.ENTERTAIN, R.color.color_bg_green, R.drawable.ic_hiburan, context.getString(R.string.hiburan), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.HOBBY, R.color.color_bg_yellow, R.drawable.ic_hobi, context.getString(R.string.hobi), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.SPORT, R.color.color_bg_purple, R.drawable.ic_olahraga, context.getString(R.string.olahraga), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.SOCIAL, R.color.color_bg_yellow, R.drawable.ic_social, context.getString(R.string.sosial), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.ETC_TRANSACTION_OUTCOME, R.color.color_bg_blue, R.drawable.ic_lain_lain, context.getString(R.string.lain_lain), CategoryType.TransactionOutcome))
-                add(CategoryEntity(CategoryKey.CASH, R.color.color_bg_green, R.drawable.ic_cash, context.getString(R.string.cash), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.ATM, R.color.color_bg_blue, R.drawable.ic_atm, context.getString(R.string.atm), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.WALLET_GREEN, R.color.color_bg_green, R.drawable.ic_wallet, context.getString(R.string.dompet), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.WALLET_BLUE, R.color.color_bg_blue, R.drawable.ic_wallet_blue, context.getString(R.string.dompet), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.WALLET_RED, R.color.color_bg_red, R.drawable.ic_wallet_red, context.getString(R.string.dompet), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.WALLET_YELLOW, R.color.color_bg_yellow, R.drawable.ic_wallet_yellow, context.getString(R.string.dompet), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.ATM_GREEN, R.color.color_bg_green, R.drawable.ic_atm_green, context.getString(R.string.atm), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.ATM_RED, R.color.color_bg_red, R.drawable.ic_atm_red, context.getString(R.string.atm), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.ATM_YELLOW, R.color.color_bg_yellow, R.drawable.ic_atm_yellow, context.getString(R.string.atm), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.ETC_ADD_INCOME, R.color.color_bg_blue, R.drawable.ic_lain_lain, context.getString(R.string.atm), CategoryType.AddIncome))
-                add(CategoryEntity(CategoryKey.SALARY, R.color.color_bg_green, R.drawable.ic_money_bag, context.getString(R.string.gaji), CategoryType.TransactionIncome))
-                add(CategoryEntity(CategoryKey.BONUS, R.color.color_bg_blue, R.drawable.ic_bonus, context.getString(R.string.bonus), CategoryType.TransactionIncome))
-                add(CategoryEntity(CategoryKey.GIFT, R.color.color_bg_yellow, R.drawable.ic_gift, context.getString(R.string.pemberian), CategoryType.TransactionIncome))
-                add(CategoryEntity(CategoryKey.BUSINESS, R.color.color_bg_green, R.drawable.ic_bisnis, context.getString(R.string.bisnis), CategoryType.TransactionIncome))
-                add(CategoryEntity(CategoryKey.INVESTMENT, R.color.color_bg_light_blue, R.drawable.ic_investasi_income, context.getString(R.string.investasi), CategoryType.TransactionIncome))
-                add(CategoryEntity(CategoryKey.SALE, R.color.color_bg_pink, R.drawable.ic_sale, context.getString(R.string.penjualan), CategoryType.TransactionIncome))
-                add(CategoryEntity(CategoryKey.ETC_TRANSACTION_INCOME, R.color.color_bg_red, R.drawable.ic_lain_lain, context.getString(R.string.lain_lain), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.FOOD, ColorBgRed.toArgb(), R.drawable.ic_food_n_drink, context.getString(R.string.makan_dan_minum), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.SHOP, ColorBgLightBlue.toArgb(), R.drawable.ic_belanja, context.getString(R.string.belanja), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.TRANSPORT, ColorBgBlue.toArgb(), R.drawable.ic_kendaraan, context.getString(R.string.kendaraan), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.BILL, ColorBgPurple.toArgb(), R.drawable.ic_tagihan, context.getString(R.string.tagihan), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.HEALTH, ColorBgGray.toArgb(), R.drawable.ic_kesehatan, context.getString(R.string.kesehatan), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.HOLIDAY, ColorBgGreen.toArgb(), R.drawable.ic_liburan, context.getString(R.string.liburan), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.PET, ColorBgPink.toArgb(), R.drawable.ic_peliharaan, context.getString(R.string.peliharaan), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.INSURANCE, ColorBgPurple.toArgb(), R.drawable.ic_asuransi, context.getString(R.string.asuransi), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.ALMS, ColorBgRed.toArgb(), R.drawable.ic_sedekah, context.getString(R.string.sedekah), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.ENTERTAIN, ColorBgGreen.toArgb(), R.drawable.ic_hiburan, context.getString(R.string.hiburan), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.HOBBY, ColorBgYellow.toArgb(), R.drawable.ic_hobi, context.getString(R.string.hobi), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.SPORT, ColorBgPurple.toArgb(), R.drawable.ic_olahraga, context.getString(R.string.olahraga), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.SOCIAL, ColorBgYellow.toArgb(), R.drawable.ic_social, context.getString(R.string.sosial), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.ETC_TRANSACTION_OUTCOME, ColorBgBlue.toArgb(), R.drawable.ic_lain_lain, context.getString(R.string.lain_lain), CategoryType.TransactionOutcome))
+                add(CategoryEntity(CategoryKey.CASH, ColorBgGreen.toArgb(), R.drawable.ic_cash, context.getString(R.string.cash), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.ATM, ColorBgBlue.toArgb(), R.drawable.ic_atm, context.getString(R.string.atm), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.WALLET_GREEN, ColorBgGreen.toArgb(), R.drawable.ic_wallet, context.getString(R.string.dompet), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.WALLET_BLUE, ColorBgBlue.toArgb(), R.drawable.ic_wallet_blue, context.getString(R.string.dompet), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.WALLET_RED, ColorBgRed.toArgb(), R.drawable.ic_wallet_red, context.getString(R.string.dompet), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.WALLET_YELLOW, ColorBgYellow.toArgb(), R.drawable.ic_wallet_yellow, context.getString(R.string.dompet), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.ATM_GREEN, ColorBgGreen.toArgb(), R.drawable.ic_atm_green, context.getString(R.string.atm), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.ATM_RED, ColorBgRed.toArgb(), R.drawable.ic_atm_red, context.getString(R.string.atm), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.ATM_YELLOW, ColorBgYellow.toArgb(), R.drawable.ic_atm_yellow, context.getString(R.string.atm), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.ETC_ADD_INCOME, ColorBgBlue.toArgb(), R.drawable.ic_lain_lain, context.getString(R.string.atm), CategoryType.AddIncome))
+                add(CategoryEntity(CategoryKey.SALARY, ColorBgGreen.toArgb(), R.drawable.ic_money_bag, context.getString(R.string.gaji), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.BONUS, ColorBgBlue.toArgb(), R.drawable.ic_bonus, context.getString(R.string.bonus), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.GIFT, ColorBgYellow.toArgb(), R.drawable.ic_gift, context.getString(R.string.pemberian), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.BUSINESS, ColorBgGreen.toArgb(), R.drawable.ic_bisnis, context.getString(R.string.bisnis), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.INVESTMENT, ColorBgLightBlue.toArgb(), R.drawable.ic_investasi_income, context.getString(R.string.investasi), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.SALE, ColorBgPink.toArgb(), R.drawable.ic_sale, context.getString(R.string.penjualan), CategoryType.TransactionIncome))
+                add(CategoryEntity(CategoryKey.ETC_TRANSACTION_INCOME, ColorBgRed.toArgb(), R.drawable.ic_lain_lain, context.getString(R.string.lain_lain), CategoryType.TransactionIncome))
             }
             return categories
         }
